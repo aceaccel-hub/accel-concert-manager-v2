6b8aa3aca3a1ec39f25c394dfd1a1862fc4603d4
aceaccel-hub <aceaccel@gmail.com>
2026-06-15 17:05:00 +0900

    chore: fix misplaced repository tracking

--- Desktop/아첼연주회관리프로그램/preload.js
+++
@@ -1,8 +0,0 @@
-// preload.js - Electron 보안 설정
-import { contextBridge } from 'electron';
-
-contextBridge.exposeInMainWorld('electron', {
-  nodeVersion: process.versions.node,
-  chromeVersion: process.versions.chrome,
-  electronVersion: process.versions.electron,
-});
